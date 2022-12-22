c78a33da9c0dcd2de11be86b060234e370d5122e
carlosrasensio <carlosrasensio@gmail.com>
2022-12-22 16:23:23 +0100

    Created new extensions for downloading images and show alerts.

---
+++ WeatherApp/WeatherApp/Utils/Extensions/UIImageVIew+URLSession.swift
@@ -0,0 +1,25 @@
+//
+//  UIImageVIew+URLSession.swift
+//  WeatherApp
+//
+//  Created by Carlos Rodriguez Asensio on 22/12/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func getImageFromURL(urlString: String) {
+        if self.image == nil {
+            self.image = UIImage(named: "icnOpenWeather")
+        }
+        URLSession.shared.dataTask(with: URL(string: urlString)!) { (data, response, error) in
+            guard error == nil else { return }
+            DispatchQueue.main.async {
+                guard let data = data else { return }
+                let image = UIImage(data: data )
+                self.image = image
+            }
+        }.resume()
+    }
+}
---
+++ WeatherApp/WeatherApp/Utils/Extensions/UIViewController+Alert.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Alert.swift
+//  WeatherApp
+//
+//  Created by Carlos Rodriguez Asensio on 22/12/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+  func showAlert(title: String, message : String) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alert.view.backgroundColor = Constants.appColor
+    alert.view.alpha = 0.5
+    alert.view.layer.cornerRadius = 15
+    present(alert, animated: true)
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+      alert.dismiss(animated: true)
+    }
+  }
+}
