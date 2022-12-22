2e4316bc56dd5ee470e3bfcaffe20c0e8cb2d11f
carlosrasensio <carlosrasensio@gmail.com>
2022-12-22 20:16:30 +0100

    Modfied constants to configure the new URL.

--- WeatherApp/WeatherApp/Utils/Constants.swift
+++ WeatherApp/WeatherApp/Utils/Constants.swift
@@ -15,19 +15,22 @@ struct Constants {
 
   struct NetworkManager {
     static let version = 2.5
+    static let forecast = "/forecast"
+    static let weather = "/weather"
     static let units = "metric"
     static let cnt = "cnt"
-    static let numberOfDays = 5
+    static let numberOfTimestamps = 5
     static let apiKey = "c2ac245c2415ab142efeba483dacb09d"
     
     struct URLs {
-      static let base = "https://api.openweathermap.org/data/\(NetworkManager.version)/weather"
+      static let base = "https://api.openweathermap.org/data/\(NetworkManager.version)"
       static let icon = "https://openweathermap.org/img/w/"
+
     }
     
     struct Optionals {
       static let units = "&units=\(NetworkManager.units)"
-      static let numberOfDays = "&cnt=\(NetworkManager.numberOfDays)"
+      static let numberOfTimestamps = "&cnt=\(NetworkManager.numberOfTimestamps)"
     }
     
     struct Endpoints {
