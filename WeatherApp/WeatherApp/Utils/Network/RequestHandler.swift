1fd2d2ce37feda3296675c30bbf10b51ee0cdf97
carlosrasensio <carlosrasensio@gmail.com>
2022-12-22 20:17:29 +0100

    Modified the configuration of the URL.

--- WeatherApp/WeatherApp/Utils/Network/RequestHandler.swift
+++ WeatherApp/WeatherApp/Utils/Network/RequestHandler.swift
@@ -16,9 +16,9 @@ final class RequestHandler: RequestHandlerProtocol {
   func getURL() -> URL {
     let location = "?q=\(Constants.location.city),\(Constants.location.countyCode)"
     let url = URL(string: Constants.NetworkManager.URLs.base +
+                  Constants.NetworkManager.forecast +
                   location +
                   Constants.NetworkManager.Optionals.units +
-                  Constants.NetworkManager.Optionals.numberOfDays +
                   Constants.NetworkManager.Endpoints.apiKey)!
     print("💻 ", url)
     
@@ -27,7 +27,7 @@ final class RequestHandler: RequestHandlerProtocol {
   
   func getRequest() -> URLRequest {
     let url = getURL()
-    var request = URLRequest(url: url)
+    let request = URLRequest(url: url)
     
     return request
   }
