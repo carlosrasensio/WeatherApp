ec54ebfe305eedf86135257285c577de58384a1b
carlosrasensio <carlosrasensio@gmail.com>
2022-12-30 22:42:09 +0100

    Added showing alert function to the main queue.

--- WeatherApp/WeatherApp/Utils/Extension/UIViewController+Alert.swift
+++ WeatherApp/WeatherApp/Utils/Extension/UIViewController+Alert.swift
@@ -9,13 +9,16 @@ import UIKit
 
 extension UIViewController {
   func showAlert(title: String, message : String) {
-    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    alert.view.backgroundColor = Constants.appColor
-    alert.view.alpha = 0.5
-    alert.view.layer.cornerRadius = 15
-    self.present(alert, animated: true)
-    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
-      alert.dismiss(animated: true)
+    DispatchQueue.main.async {
+      let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+      alert.view.backgroundColor = Constants.appColor
+      alert.view.alpha = 0.5
+      alert.view.layer.cornerRadius = 15
+      self.present(alert, animated: true)
+      
+      DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+        alert.dismiss(animated: true)
+      }
     }
   }
 }
