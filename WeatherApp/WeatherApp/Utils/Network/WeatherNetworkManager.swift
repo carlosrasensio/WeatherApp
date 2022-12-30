1b72f985284579d9345d732b566f5ed65d06d2f5
carlosrasensio <carlosrasensio@gmail.com>
2022-12-30 22:41:39 +0100

    Class reconfiguration.

--- WeatherApp/WeatherApp/Utils/Network/WeatherNetworkManager.swift
+++ WeatherApp/WeatherApp/Utils/Network/WeatherNetworkManager.swift
@@ -12,37 +12,36 @@ protocol WeatherNetworkManagerProtocol {
   func getLocationWeather() async throws -> Forecast
 }
 
-final class WeatherNetworkManager: WeatherNetworkManagerProtocol, NetworkManagerProtocol {
+final class WeatherNetworkManager: WeatherNetworkManagerProtocol {
   let requestHandler = RequestHandler()
 
-  func getLocationWeather() async throws -> Forecast {
-    try await request(url: requestHandler.getURL())
-  }
-  
-  func request<Forecast>(url: URL) async throws -> Forecast {
-    // TODO
-  }
-  
-  
-  
-  
 //  func getLocationWeather() async throws -> Forecast {
-//    let url = requestHandler.getURL()
-//    let (data, response) = try await URLSession.shared.data(from: url)
-//
-//    if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
-//      print("\n❌ Server Error: response status code \(response.statusCode)")
-//      throw NetworkError.serverError(statusCode: response.statusCode)
-//    }
-//
-//    do {
-//      let decoder = JSONDecoder()
-//      let locationWeather = try decoder.decode(Forecast.self, from: data)
-//
-//      return locationWeather
-//    } catch {
-//      print("\n❌ Decoding Error: \(error.localizedDescription)")
-//      throw NetworkError.decodingError(error)
-//    }
+//    try await request(url: requestHandler.getURL())
 //  }
+  
+  func getLocationWeather() async throws -> Forecast {
+    let url = requestHandler.getURL()
+    let (data, response) = try await URLSession.shared.data(from: url)
+
+    if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+      print("\n❌ Server Error: response status code \(response.statusCode)")
+      throw NetworkError.serverError(statusCode: response.statusCode)
+    }
+
+    do {
+      let decoder = JSONDecoder()
+      let locationWeather = try decoder.decode(Forecast.self, from: data)
+
+      return locationWeather
+    } catch {
+      print("\n❌ Decoding Error: \(error.localizedDescription)")
+      throw NetworkError.decodingError(error)
+    }
+  }
 }
+
+//extension WeatherNetworkManager: NetworkManagerProtocol {
+//    func request<Forecast>(url: URL) async throws -> Forecast {
+//      // TODO
+//    }
+//}
